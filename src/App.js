c1c60955f44249ac608c7dc6f671dfb202048640
Belde, Vikram <belvikram@gmail.com>
2017-01-08 19:59:09 -0500

    Navigate to new Page using router-link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css'
+import { Link } from 'react-router'
+
 class App extends Component {
   render() {
     return (
@@ -7,9 +9,11 @@ class App extends Component {
           <div className='subContainer'>
               <div className='page1Cont'>
                   <button className='button1' onClick={(e)=>{console.log('onCLicke Triggered',e.type)}}> PAGE 1 </button>
+                  <Link to="/Page1">Page1</Link>
               </div>
               <div className='page2Cont'>
                   <button  className='button2'> PAGE 2 </button>
+                  <Link to="/Page2">Page2</Link>
               </div>
           </div>
       </div>
--- src/Page2.js
+++ src/Page2.js
@@ -5,8 +5,8 @@ class Page2 extends Component {
     return (
       <div className='mainContainer'>
           <div className='subContainer'>
-              <div className='page1Cont'>
-                  <button className='button1' onClick={(e)=>{console.log('onCLicke Triggered',e.type)}}> PAGE 2 </button>
+              <div className='page2Cont'>
+                  <button className='button2' onClick={(e)=>{console.log('onCLicke Triggered',e.type)}}> PAGE 2 </button>
               </div>
           </div>
       </div>
